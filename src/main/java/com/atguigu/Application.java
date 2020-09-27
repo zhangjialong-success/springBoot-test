68f94dfeaf39e92556f6083eadb96b8660933fbf
郑帅哥 <1608200596@qq.com>
2020-09-27 19:20:31 +0800

    z郑帅哥啊

--- src/main/java/com/atguigu/Application.java
+++ src/main/java/com/atguigu/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
-
         System.out.println(222);
         System.out.println("郑帅哥到此一游");
     }
